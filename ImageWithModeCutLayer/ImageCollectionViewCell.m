d9095432a73ef5774ef4b1600af19d7e661345b0
陈宇峰 <yufengcode@gmail.com>
2016-06-30 10:26:55 +0800

    pod Masonry

---
+++ ImageWithModeCutLayer/ImageCollectionViewCell.m
@@ -0,0 +1,33 @@
+//
+//  ImageCollectionViewCell.m
+//  ImageWithModeCutLayer
+//
+//  Created by chenyufeng on 6/30/16.
+//  Copyright © 2016 chenyufengweb. All rights reserved.
+//
+
+#import "ImageCollectionViewCell.h"
+
+@interface ImageCollectionViewCell()
+
+@property (nonatomic, strong) UIImageView *imageView;
+
+@end
+
+@implementation ImageCollectionViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+
+        
+
+
+
+
+    }
+    return self;
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,4 @@
+target "ImageWithModeCutLayer" do
+       platform:ios,'8.0'
+       pod "Masonry" ,'~>1.0.1'
+end
