212ef814c3df5f9b16ab5c96b6df8e06f76fbe21
陈宇峰 <yufengcode@gmail.com>
2016-06-30 09:40:46 +0800

    Build proj

---
+++ ImageWithModeCutLayer/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImageWithModeCutLayer
+//
+//  Created by chenyufeng on 6/30/16.
+//  Copyright © 2016 chenyufengweb. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+/**
+ *  讲解关于图片的填充模式contentMode,图片裁剪；
+    和View的Layer属性讲解
+ */
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+@end
